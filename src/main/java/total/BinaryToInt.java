6f03fc31b764f807ec0ac0556df20545f6b09634
AnnaVasilisa <AnnaVasilets1211@gmail.com>
2019-06-06 23:11:40 +0300

    Переделано 1 итоговое задание

--- src/main/java/total/BinaryToInt.java
+++ src/main/java/total/BinaryToInt.java
@@ -2,13 +2,23 @@ package total;
 
 public class BinaryToInt {
     public static void main(String[] args) {
-        int translatable = 156, resultNumber;
-        StringBuilder sb = new StringBuilder();
-        while (translatable != 0) {
-            resultNumber = translatable % 2;
-            sb.append(resultNumber);
-            translatable = translatable / 2;
+        int decimalInt = 10011;
+        String str = String.valueOf(decimalInt);
+        int sum = 0, a;
+        char c;
+
+        for (int i = 0; i < str.length(); i++) {
+            c = str.charAt(str.length() - 1 - i);
+            if (c == '1' || c == '0') {
+                if (c == '1')
+                    a = 1;
+                else a = 0;
+            } else {
+                System.out.println("Неверный ввод!");
+                break;
+            }
+            sum += (a * Math.pow(2, i));
         }
-        System.out.println(sb);
+        System.out.println("\"" + str + "\" -> " + sum);
     }
 }
